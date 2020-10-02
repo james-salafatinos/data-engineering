1fe5c476c06a37b1448726f359ab4286ede68519
jfs <jamessalafatinos@outlook.com>
2020-10-01 19:45:23 -0500

    second assignment on selection sort

--- algos.py
+++ algos.py
@@ -2,7 +2,7 @@ class Algos:
     def __init__(self):
         pass
     
-    def binary_search(arr, item, log = False):
+    def binary_search(arr: list, item) -> int:
         """
         Returns the index of the item if found, otherwise None. Time-complexity = O(log n)
         Parameters
@@ -24,10 +24,10 @@ class Algos:
                 low_idx = mid_idx + 1
             else:
                 high_idx = mid_idx - 1
-        return None
+        return -1
     
     
-    def linear_search(arr, item):
+    def linear_search(arr: list, item) -> int:
         """
         Returns the index of item in array if found, otherwise None. Time-complexity = O(n)
         Parameters
@@ -42,9 +42,9 @@ class Algos:
                 return i
             else:
                 continue
-        return None
+        return -1
     
-    def qsort(arr):
+    def qsort(arr: list) -> list:
         """
         Returns a sorted array. Time-complexity = O(n log n)
         Parameters
@@ -61,3 +61,20 @@ class Algos:
             less = [i for i in arr[1:] if i <= pivot]
             greater = [i for i in arr[1:] if i >= pivot]
             return qsort(less) + [pivot] + qsort(greater)
+        
+    def selection_sort(arr: list) -> list:
+        """
+        Returns a sorted array in O(n^2) time
+        ----------
+            arr : list, array
+                the array to sort
+        """
+        for i in range(0, len(arr)-1):
+            small = i
+            for j in range(i+1, len(arr)):
+                if arr[j] < arr[small]:
+                    small = j
+            temp_i = arr[i]
+            arr[i] = arr[small]
+            arr[small] = temp_i
+        return arr
